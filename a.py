05936de6e8356a562d5039e887d34ec8dc828158
kshravi86 <kshravi86@gmail.com>
2016-12-25 14:40:34 +0530

    change

--- a.py
+++ a.py
@@ -4,5 +4,5 @@ driver.set_window_size(1120, 550)
 driver.get("https://www.eventshigh.com/chennai/this+weekend")
 #driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
 #driver.find_element_by_id("search_button_homepage").click()
-print driver.current_url
+print(driver.current_url)
 driver.quit()
