02c7437cfb4c03f44b4f88b91987aae7b4480ecb
kshravi86 <kshravi86@gmail.com>
2016-12-25 22:25:37 +0530

    js

--- a.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-driver = webdriver.PhantomJS()
-#driver.set_window_size(1120, 550)
-driver.get("https://www.google.com")
-#river.find_element_by_name('events').send_keys("realpython")
-#river.find_element_by_class_name("home-cat-name-mob").click()
-print(driver.current_url)
-driver.quit()
