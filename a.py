5f491beb4c10904c009ca4239a21f7e183259609
kshravi86 <kshravi86@gmail.com>
2016-12-25 19:34:03 +0530

    search

--- a.py
+++ a.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 driver = webdriver.PhantomJS()
 driver.set_window_size(1120, 550)
 driver.get("https://www.eventshigh.com")
-driver.find_element_by_id('searchInterestHeaderInputMob').send_keys("realpython")
+driver.find_element_by_name('events').send_keys("realpython")
 #river.find_element_by_class_name("home-cat-name-mob").click()
 print(driver.current_url)
 driver.quit()
