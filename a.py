0d02ab9a70cfecfb8757757a17ff672b4bcbb8cc
kshravi86 <kshravi86@gmail.com>
2016-12-25 18:38:55 +0530

    click

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 driver = webdriver.PhantomJS()
 driver.set_window_size(1120, 550)
-driver.get("https://www.eventshigh.com/chennai/this+weekend")
-#driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
-#driver.find_element_by_id("search_button_homepage").click()
+driver.get("https://www.eventshigh.com")
+#river.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+driver.find_element_by_id("chennai").click()
 print(driver.current_url)
 driver.quit()
