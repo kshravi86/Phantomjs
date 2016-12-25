a128dc5c6586cd58fc7463963fac500f4bf64a55
kshravi86 <kshravi86@gmail.com>
2016-12-25 20:49:19 +0530

    google

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
-driver.set_window_size(1120, 550)
-driver.get("https://www.eventshigh.com")
-driver.find_element_by_name('events').send_keys("realpython")
+driver = webdriver.PhantomJS()
+#driver.set_window_size(1120, 550)
+driver.get("https://www.google.com")
+#river.find_element_by_name('events').send_keys("realpython")
 #river.find_element_by_class_name("home-cat-name-mob").click()
 print(driver.current_url)
 driver.quit()
