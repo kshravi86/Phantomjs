24cc05eaea9f8e9dc5b0346688efc94bf2a782ac
kshravi86 <kshravi86@gmail.com>
2016-12-26 15:00:08 +0530

    js

--- test.js
+++ test.js
@@ -17,3 +17,15 @@ var http = require("http");
 	  
   
   console.log('Server running at http://127.0.0.1:8081/');
+  
+  var f=require("fs");
+  
+  f.readFile('input.txt',function(err,data){
+	  
+	  if(err){
+		console.log(  err.stack);
+		return;
+		  
+	  }
+	  console.log(data.toString);
+  } );
