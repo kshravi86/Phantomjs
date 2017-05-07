502d22f79df8ba1520aa916bcc4e64bd239eda1c
kshravi86 <kshravi86@gmail.com>
2017-05-07 09:48:16 +0530

    y

--- con.py
+++ con.py
@@ -3,6 +3,7 @@ from bokeh.layouts import gridplot
 from bokeh.plotting import figure, output_file, show
 
 N = 100
-x = np.linspace(0, 4*np.pi, N) y0 = np.sin(x) y1 = np.cos(x)
+x = np.linspace(0, 4*np.pi, N) 
+y0 = np.sin(x) y1 = np.cos(x)
 y2 = np.sin(x) + np.cos(x)
 
