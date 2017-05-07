75c735c27338851211e4d96dcdf41078677a564e
kshravi86 <kshravi86@gmail.com>
2017-05-07 09:44:41 +0530

    gg

--- con.py
+++ con.py
@@ -3,7 +3,7 @@ from bokeh.plotting import figure, output_file, show
 x = [1, 2, 3, 4, 5]
 y = [23, 15, 7, 12, 21]
 
-output_file(“lines.html”)
+
 
 p = figure(title=”Bokeh Demo for OSFY”, x_axis_label=’x’, y_axis_label=’y’)
 
