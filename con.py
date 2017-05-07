b74c22e93bdf4e576ece6239fa81d3604feaf848
kshravi86 <kshravi86@gmail.com>
2017-05-07 09:50:08 +0530

    tf

--- con.py
+++ con.py
@@ -4,6 +4,8 @@ from bokeh.plotting import figure, output_file, show
 
 N = 100
 x = np.linspace(0, 4*np.pi, N) 
-y0 = np.sin(x) y1 = np.cos(x)
+y0 = np.sin(x) 
+y1 = np.cos(x)
 y2 = np.sin(x) + np.cos(x)
+output_file(“linked_panning.html”)
 
